58fd5ffbb4b9545e8609767c55927760ea645ec0
Andreivan <andreivan.santos@gmail.com>
2022-05-18 14:17:09 -0300

    Get element from selector file

--- cypress/integration/filters-todos/footer-filters.spec.js
+++ cypress/integration/filters-todos/footer-filters.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import { faker } from '@faker-js/faker';
+import * as elements from '../../selectors/global-selectors'
 
 describe('As an user, I want to filter todos so that I can view the filtered todos', () => {
 
@@ -12,11 +13,11 @@ describe('As an user, I want to filter todos so that I can view the filtered tod
 
     it('Filter Active todos', () => {
         cy.get('a[href*="active"]').click();
-        cy.get('.todo-list li').should('have.length', 3)
+        cy.get(elements.listTodo).should('have.length', 3)
     });
 
     it('Filter Completed todos', () => {
         cy.get('a[href*="completed"]').click();
-        cy.get('.todo-list li').should('have.length', 2)
+        cy.get(elements.listTodo).should('have.length', 2)
     });
   })
